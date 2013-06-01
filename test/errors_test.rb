3dd9800a707884c93c304127181c78ba7072c7ab
tonchis <lucas.tolchinsky@gmail.com>
2013-05-31 23:10:43 -0300

    Adds errors#full_messages and errors#on.

---
+++ test/errors_test.rb
@@ -0,0 +1,27 @@
+require_relative 'support/helper'
+require_relative 'support/address'
+
+class ErrorsTest < Test::Unit::TestCase
+  def initialize(*args)
+    @address = Address.hatch(city: 'Buenos Aires', street: '', number: -1)
+    super
+  end
+
+  def test_errors_on
+    assert @address.errors.on(:city).empty?
+    assert_equal 'Address must have a street', @address.errors.on(:street)
+    assert_equal 'Address must have a positive number', @address.errors.on(:number)
+  end
+
+  def test_hash_accessor
+    assert @address.errors[:city].empty?
+    assert_equal 'Address must have a street', @address.errors[:street]
+    assert_equal 'Address must have a positive number', @address.errors[:number]
+  end
+
+  def test_errors_full_messages
+    assert @address.errors.full_messages.include?('Address must have a street')
+    assert @address.errors.full_messages.include?('Address must have a positive number')
+  end
+end
+
