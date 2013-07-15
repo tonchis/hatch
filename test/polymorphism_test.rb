a20d817f6022c4fecb7f5e12618ace4e4e07c10e
tonchis <lucas.tolchinsky@gmail.com>
2013-07-15 12:40:17 -0300

    Uses instance_of? instead of is_a?.

--- test/polymorphism_test.rb
+++ test/polymorphism_test.rb
@@ -4,13 +4,13 @@ require_relative 'support/address'
 class PolymorphismTest < Test::Unit::TestCase
   def test_polymorphism
     address = Address.hatch(street: "Fake St", number: 1234, city: "Buenos Aires")
-    assert address.is_a?(Address)
+    assert address.instance_of?(Address)
     assert_equal address.street, "Fake St"
     assert_equal address.number, 1234
     assert_equal address.city,   "Buenos Aires"
 
     address = Address.hatch(street: "Fake St", number: -1, city: "Buenos Aires")
-    assert address.is_a?(Address::InvalidAddress)
+    assert address.instance_of?(Address::InvalidAddress)
     assert_equal address.street, "Fake St"
     assert_equal address.number, -1
     assert_equal address.city,   "Buenos Aires"
--- test/validation_test.rb
+++ test/validation_test.rb
@@ -4,21 +4,21 @@ require_relative 'support/address'
 class ValidationTest < Test::Unit::TestCase
   def test_valid
     address = Address.hatch(street: 'Fake St', city: 'Buenos Aires', number: 1234)
-    assert address.is_a?(Address)
+    assert address.instance_of?(Address)
     assert address.respond_to?(:valid?)
     assert address.valid?
   end
 
   def test_invalid
     address = Address.hatch(city: 'Buenos Aires', street: '', number: 1234)
-    assert address.is_a?(Address::InvalidAddress)
+    assert address.instance_of?(Address::InvalidAddress)
     assert address.respond_to?(:valid?)
     assert !address.valid?
   end
 
   def test_ignore_attributes
     address = Address.hatch(street: 'Fake St', city: 'Buenos Aires', number: 1234, sorry: :oops)
-    assert address.is_a?(Address)
+    assert address.instance_of?(Address)
     assert address.valid?
   end
 end
